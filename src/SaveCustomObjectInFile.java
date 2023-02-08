a59b3715065ee7df01d70be7ebede689ef3e84b0
Rushi Bhatt <rybhatt27@gmail.com>
2023-02-08 15:44:37 -0800

    project setup

---
+++ src/SaveCustomObjectInFile.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+
+public class SaveCustomObjectInFile implements Serializable {
+	
+	final static String modelPath = "D:/GitHub/ML_piezoelectric_prediction/models/model.save";
+	
+	
+	private static final long serialVersionUID = -1L;
+    public MLClassifier loadObject(MLClassifier classifier) {
+    	MLClassifier crs = new MLClassifier();
+        try(ObjectInputStream source = new ObjectInputStream(new FileInputStream(modelPath))){
+            crs = (MLClassifier) source.readObject();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return crs;
+    }
+
+    public void saveObject(MLClassifier classifier) {
+        try(ObjectOutputStream destination = new ObjectOutputStream(new FileOutputStream(modelPath))){
+            destination.writeObject(classifier);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
